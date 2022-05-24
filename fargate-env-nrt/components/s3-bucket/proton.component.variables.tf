e42186e20bd14a2024aeff57a712044c2b2daeb9
sbx_user1051 <sbx_user1051@169.254.117.93>
2022-05-24 15:20:03 +0000

    Update requested for Proton - component/s3-bucket

---
+++ fargate-env-nrt/components/s3-bucket/proton.component.variables.tf
@@ -0,0 +1,36 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:ap-northeast-1:259837135622:component/s3-bucket
+
+If the resource is no longer accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
+variable "component" {
+  type = object({
+    name = string
+  })
+}
+
+variable "environment" {
+  type = object({
+    account_id = string
+    name       = string
+    outputs    = map(string)
+  })
+}
+
+variable "service" {
+   type = object({
+     name = string
+   })
+   default = null
+}
+
+variable "service_instance" {
+  type = object({
+    name   = string
+    inputs = map(string)
+  })
+  default = null
+}
